0afa325c078b6513d1351d29989d48cbef0ca6e8
VladimirYastrebov <vladimir.i.yastrebov@gmail.com>
2026-07-16 11:35:21 +0300

    feat: add warp technology

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
 # run.py
 
 #main bot
-from my_bot import main
-
-if __name__ == "__main__":
-    main()
-
-#warp units
-# from tests.tech.test_warp import main
+# from my_bot import main
 
 # if __name__ == "__main__":
 #     main()
+
+#warp units
+from tests.tech.test_warp import main
+
+if __name__ == "__main__":
+    main()
 
 #use chrono boost on Nexus
 # from tests.macro.chrono_boost import main
--- tests/tech/test_warp.py
+++ tests/tech/test_warp.py
@@ -11,6 +11,20 @@ from sc2.position import Point2
 class TestWarpBot(BotAI):
     """Bot to test Warp Gate functionality."""
 
+    async def warp_new_units(self):
+        for warpgate in self.structures(UnitTypeId.WARPGATE).ready:
+            abilities = await self.get_available_abilities([warpgate])
+            # all the units have the same cooldown anyway so let's just look at ZEALOT
+            if AbilityId.WARPGATETRAIN_STALKER in abilities[0]:
+                pylon = self.structures(UnitTypeId.PYLON).ready.random
+                pos = pylon.position.to2.random_on_distance(4)
+                placement = await self.find_placement(AbilityId.WARPGATETRAIN_STALKER, pos, placement_step=1)
+                if placement is None:
+                    # return ActionResult.CantFindPlacementLocation
+                    print("can't place")
+                    return
+                warpgate.warp_in(UnitTypeId.STALKER, placement)
+
     async def on_step(self, iteration: int):
         await self.distribute_workers()
 
@@ -47,28 +61,28 @@ class TestWarpBot(BotAI):
             self.research(UpgradeId.WARPGATERESEARCH)
 
         if iteration % 50 == 0:
-            await self.try_warp()
+            await self.warp_new_units()
 
         if not self.structures(UnitTypeId.WARPGATE).ready:
             for gateway in self.structures(UnitTypeId.GATEWAY).ready:
                 if gateway.is_idle and self.can_afford(UnitTypeId.ZEALOT):
                     gateway.train(UnitTypeId.ZEALOT)
 
-    async def try_warp(self):
-        """Attempt to warp in a Zealot."""
-        warpgates = [gate for gate in self.structures(UnitTypeId.WARPGATE).ready if gate.is_idle]
-        pylons = [pylon for pylon in self.structures(UnitTypeId.PYLON).ready if pylon.is_ready]
+    # async def try_warp(self):
+    #     """Attempt to warp in a Zealot."""
+    #     warpgates = [gate for gate in self.structures(UnitTypeId.WARPGATE).ready if gate.is_idle]
+    #     pylons = [pylon for pylon in self.structures(UnitTypeId.PYLON).ready if pylon.is_ready]
 
-        if not warpgates or not pylons:
-            return
-        if not self.can_afford(UnitTypeId.ZEALOT) or self.supply_left <= 0:
-            return
+    #     if not warpgates or not pylons:
+    #         return
+    #     if not self.can_afford(UnitTypeId.ZEALOT) or self.supply_left <= 0:
+    #         return
 
-        for pylon in pylons:
-            target_position = pylon.position
-            if self.can_place(UnitTypeId.ZEALOT, target_position):
-                warpgates[0].warp_in(UnitTypeId.ZEALOT, target_position)
-                return
+    #     for pylon in pylons:
+    #         target_position = pylon.position
+    #         if self.can_place(UnitTypeId.ZEALOT, target_position):
+    #             warpgates[0].warp_in(UnitTypeId.ZEALOT, target_position)
+    #             return
 
 def main():
     run_game(
